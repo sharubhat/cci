d750067101ca2ec0a54f97425182d5af8014f01d
Sharath Mahadev Bhat <sharubhat@gmail.com>
2014-08-16 17:19:34 -0700

    Triangular number series solved

---
+++ src/test/java/c09_recursion_and_dp/additional_problems/TriangularNumbersTest.java
@@ -0,0 +1,43 @@
+package c09_recursion_and_dp.additional_problems;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by sharath on 8/16/14.
+ */
+public class TriangularNumbersTest {
+    TriangularNumbers tn;
+    @Before
+    public void setUp() {
+        tn = new TriangularNumbers();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeNum() {
+        tn.nthTNumber(-4);
+    }
+
+    @Test
+    public void testPositiveNum() {
+        assertEquals("nth number of triagle series", 21, tn.nthTNumber(6));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegNum() {
+        tn.nthTNum(-4);
+    }
+
+    @Test
+    public void testPosNum() {
+        assertEquals("tail recursion - nth number of triagle series", 21, tn.nthTNum(6));
+    }
+
+    @After
+    public void tearDown() {
+        tn = null;
+    }
+}
