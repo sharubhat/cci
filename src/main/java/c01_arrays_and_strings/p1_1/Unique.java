3239d1ea3a4ad2800d400862884e48c20411dca7
Sharath Bhat <sharubhat@gmail.com>
2014-10-27 16:35:11 -0700

    Alternate approach

--- src/main/java/c01_arrays_and_strings/p1_1/Unique.java
+++ src/main/java/c01_arrays_and_strings/p1_1/Unique.java
@@ -1,5 +1,8 @@
 package c01_arrays_and_strings.p1_1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by sharath on 7/31/14.
  *
@@ -11,6 +14,7 @@ package c01_arrays_and_strings.p1_1;
  *    If yes, there are only 256 characters
  */
 public class Unique {
+    // Runs in O(n) but need to know the character set before hand to decide size of array
     public boolean hasAllUniqueChars(String input) {
         if(input != null && !input.isEmpty() && input.length() <= 265) {
             boolean[] charSet = new boolean[256];
@@ -20,6 +24,19 @@ public class Unique {
                     return false;
                 else
                     charSet[c] = true;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    // probably a little more of space but need not worry about character set
+    public boolean hasAllUniqueCharsUsingSet(String input) {
+        if(input != null && !input.isEmpty()) {
+            Set<Character> store = new HashSet<>();
+            for (int i = 0; i < input.length(); i++) {
+                if (!store.add(input.charAt(i)))
+                    return false;
             }
             return true;
         }
