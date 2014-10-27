f06b488d974b3c1414da135c40757478ee723048
Sharath Bhat <sharubhat@gmail.com>
2014-10-27 15:23:38 -0700

    Usage of set does not handle duplicates in first array. Replaced it with List implementation. Looks better now\!

--- src/main/java/c01_arrays_and_strings/additional_problems/SubtractStringArray.java
+++ src/main/java/c01_arrays_and_strings/additional_problems/SubtractStringArray.java
@@ -1,22 +1,24 @@
 package c01_arrays_and_strings.additional_problems;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by sharath on 8/28/14.
  */
 public class SubtractStringArray {
+
     public void subtract(String[] s1, String[] s2) {
-        Set<String> set1 = new HashSet<>(Arrays.asList(s1));
-        Set<String> set2 = new HashSet<>(Arrays.asList(s2));
-        set1.removeAll(set2);
-        System.out.println(set1);
+        // Arrays.list returns a List. This is an interface type.
+        // It can't be type casted to any concrete type such as ArrayList
+        // So you need to create a new ArrayList from the list returned.
+        List<String> list1 = new ArrayList<>(Arrays.asList(s1));
+        List<String> list2 = new ArrayList<>(Arrays.asList(s2));
+        list1.removeAll(list2);
+        System.out.println(list1);
     }
 
     public static void main(String[] args) {
-        String e1[] = { "Ac", "Bc", "Cc", "Dc", "Ec" };
+        String e1[] = { "Ac", "Bc", "Cc", "Dc", "Ec", "Ac" };
         String e2[] = new String[] { "Bc", "Dc", "Fc", "Gc", "1c", "2c", "3c", "4c" };
         new SubtractStringArray().subtract(e1, e2);
     }
