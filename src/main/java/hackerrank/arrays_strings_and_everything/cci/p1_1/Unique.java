7373a11657ca95275dff2391a4983b0f23325c95
Sharath Bhat <sharubhat@gmail.com>
2016-06-04 23:10:54 -0700

    Cracking coding interview problems

--- src/main/java/hackerrank/arrays_strings_and_everything/cci/p1_1/Unique.java
+++ src/main/java/hackerrank/arrays_strings_and_everything/cci/p1_1/Unique.java
@@ -11,13 +11,17 @@ import java.util.Set;
  *
  * Questions to ask:
  * 1. Are all characters ASCII?
- *    If yes, there are only 256 characters
+ *    If yes, there are only 128 characters(256 in extended ascii)
  */
 public class Unique {
-    // Runs in O(n) but need to know the character set before hand to decide size of array
+    /*
+        Runs in O(n) time complexity and O(k) space, k being number of characters in the character set.
+        Assumption that character set is ascii is fine because whatever is the character set, it would be
+        consisting of fixed number of characters and this logic holds good.
+    */
     public boolean hasAllUniqueChars(String input) {
-        if(input != null && !input.isEmpty() && input.length() <= 265) {
-            boolean[] charSet = new boolean[256];
+        if(input != null && !input.isEmpty() && input.length() <= 128) {
+            boolean[] charSet = new boolean[128];
             for (int i = 0; i < input.length(); i++) {
                 int c = input.charAt(i);
                 if (charSet[c])
@@ -30,7 +34,7 @@ public class Unique {
         return false;
     }
 
-    // probably a little more of space but need not worry about character set
+    // worst case still requires O(k) space
     public boolean hasAllUniqueCharsUsingSet(String input) {
         if(input != null && !input.isEmpty()) {
             Set<Character> store = new HashSet<>();
@@ -43,4 +47,23 @@ public class Unique {
         return false;
     }
 
+    /*
+        Assuming characters are all lower case and are between a to z,
+        we could save space by using a bit vector.
+     */
+    boolean isUniqueChars(String s) {
+        if(s != null && !s.isEmpty()) {
+            int checker = 0b0;
+            for (int i = 0; i < s.length(); i++) {
+                int val = s.charAt(i) - 'a';
+                // check if bit is set at position val, set if not already
+                if ((checker & (0b1 << val)) > 0) {
+                    return false;
+                }
+                checker |= (0b1 << val);
+            }
+            return true;
+        }
+        return false;
+    }
 }
