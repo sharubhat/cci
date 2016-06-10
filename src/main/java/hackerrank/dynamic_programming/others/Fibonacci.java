7a8af7fd3b9808533351d5b2793fb948ac067316
Sharath Bhat <sharubhat@gmail.com>
2016-06-10 08:50:24 -0700

    Fibonacci updated to use constant space.

--- src/main/java/hackerrank/dynamic_programming/others/Fibonacci.java
+++ src/main/java/hackerrank/dynamic_programming/others/Fibonacci.java
@@ -1,10 +1,21 @@
 package hackerrank.dynamic_programming.others;
 
 /**
+ * Time complexity of recursive solution is O(2^n). Each function call in recursion is called a recursion frame.
+ * Each frame takes order 1 time. So, time complexity is size of the tree which is 2^n.
+ *
+ * Space complexity : height of recursion tree which is O(n) - each level in recursion tree takes up one stack frame.
+ *
+ * A slight improvement comes from adding a cache called fib array and changing the algorithm to iterative.
+ * This still has O(n) space complexity.
+ *
+ * Since we are only need last two fib values, we can get rid of cache and use variables to store
+ * the two values which will offer constant space complexity.
+ *
  * Created by sharath on 9/28/15.
  */
 public class Fibonacci {
-    public long fib(int n) {
+    public static long fib(int n) {
         int[] fibStore = new int[n+1];
         fibStore[0] = 0;
         fibStore[1] = 1;
@@ -14,8 +25,20 @@ public class Fibonacci {
         return fibStore[n];
     }
 
-    // saving on space
-//    public long fibonacci(int n) {
-//
-//    }
+    public static long fib2(int n) {
+        int result = 0;
+        int fiba = 0;
+        int fibb = 1;
+        for(int i = 2; i < n+1; i++) {
+            result = fiba + fibb;
+            fiba = fibb;
+            fibb = result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(10));
+        System.out.println(fib2(10));
+    }
 }
