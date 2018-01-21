b5b8492bd1eee6abed7e01f27648d13fe2c1dc12
SHARATH BHAT <bhatshar@us.ibm.com>
2018-01-21 12:13:21 -0800

    Improved inorder successor of BST

--- src/main/java/hackerrank/trees/basics/InOrderSuccessor.java
+++ src/main/java/hackerrank/trees/basics/InOrderSuccessor.java
@@ -29,30 +29,27 @@ public class InOrderSuccessor {
 
     public static Node inorderSuccessor(Node root, int k) {
         Node successor = null;
-        if(root != null) {
-            Node curr = root;
-            while(curr.data != k) {
-                if(curr.data > k) {
-                    successor = curr;
-                    curr = curr.left;
-                } else {
-                    curr = curr.right;
-                }
-            }
-            if(curr != null && curr.right != null) {
-                successor = findMin(curr.right);
+        while(root != null) {
+            if (root.data > k) {
+                successor = root;
+                root = root.left;
+            } else if (root.data <= k) {
+                root = root.right;
             }
         }
         return successor;
     }
 
-    private static Node findMin(Node root) {
-        if(root != null) {
-            while(root.left != null) {
-                root = root.left;
+    public static Node successorRec(Node root, int k) {
+        if (root != null) {
+            if (root.data > k) {
+                Node successor = successorRec(root.left, k);
+                return successor == null ? root : successor;
+            } else {
+                return successorRec(root.right, k);
             }
         }
-        return root;
+        return null;
     }
 
     public static void main(String[] args) {
@@ -65,6 +62,36 @@ public class InOrderSuccessor {
         System.out.println("inorderSuccessor of 30 is " + inorderSuccessor(root, 30));
         System.out.println("inorderSuccessor of 35 is " + inorderSuccessor(root, 35));
         System.out.println("inorderSuccessor of 40 is " + inorderSuccessor(root, 40));
+
+        Node root2 = createBST2();
+        System.out.println("inorderSuccessor of 8 is " + inorderSuccessor(root2, 8));
+        System.out.println("inorderSuccessor of 15 is " + inorderSuccessor(root2, 15));
+        root2 = createBST2();
+        System.out.println("inorderSuccessor of 8 is " + successorRec(root2, 8));
+        System.out.println("inorderSuccessor of 15 is " + successorRec(root2, 15));
+    }
+
+    private static Node createBST2() {
+        Node root = new Node(15);
+        Node l1 = new Node(10);
+        Node r1 = new Node(20);
+        Node l1l2 = new Node(5);
+        Node r1l1 = new Node(17);
+        Node r1r2 = new Node(25);
+        Node l1l2l3 = new Node(1);
+        Node l1l2r3 = new Node(7);
+        Node l1l2r3r4 = new Node(8);
+
+        root.left = l1;
+        root.right = r1;
+        l1.left = l1l2;
+        l1l2.left = l1l2l3;
+        l1l2.right = l1l2r3;
+        l1l2r3.right = l1l2r3r4;
+        r1.left = r1l1;
+        r1.right = r1r2;
+
+        return root;
     }
 
     private static Node createBST() {
