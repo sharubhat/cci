ce2e5f05bde82aedee00bc90747daf1794b8a07b
SHARATH BHAT <shabhat@adobe.com>
2021-06-06 18:19:29 -0700

    Fixing code

--- src/main/java/threads_book/Print.java
+++ src/main/java/threads_book/Print.java
@@ -5,34 +5,34 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by sharath on 3/29/15.
- *
+ * <p>
  * Threads should print alternatively.
  */
 public class Print implements Runnable {
-    private final AtomicInteger count = new AtomicInteger(0);
+  private final AtomicInteger count = new AtomicInteger(0);
 
-    @Override
-    public void run() {
-        while(count.get() < 11) {
-            synchronized (this) {
-                System.out.printf("%s  - %d\n", Thread.currentThread().getName(), count.get());
-                count.incrementAndGet();
-            }
-            // If there is no sleep then thread which has first acquired mutex will start processing the
-            // next instruction before another thread can acquire the lock.
-            try {
-                TimeUnit.SECONDS.sleep(4);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+  @Override
+  public void run() {
+    while (count.get() < 11) {
+      synchronized (this) {
+        System.out.printf("%s  - %d%n", Thread.currentThread().getName(), count.get());
+        count.incrementAndGet();
+      }
+      // If there is no sleep then thread which has first acquired mutex will start processing the
+      // next instruction before another thread can acquire the lock.
+      try {
+        TimeUnit.SECONDS.sleep(1);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
     }
+  }
 
-    public static void main(String[] args) {
-        Print p = new Print();
-        Thread t1 = new Thread(p, "Thread 1");
-        Thread t2 = new Thread(p, "Thread 2");
-        t1.start();
-        t2.start();
-    }
+  public static void main(String[] args) {
+    Print p = new Print();
+    Thread t1 = new Thread(p, "Thread 1");
+    Thread t2 = new Thread(p, "Thread 2");
+    t1.start();
+    t2.start();
+  }
 }
